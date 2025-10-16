33cd35490534c4cdc5c33a3fc179f9b3dba93b78
Emmanuel Steve <immsteve70@gmail.com>
2025-10-16 09:01:06 +0800

    Modified

--- miniprojects/loginsystem.py
+++ miniprojects/loginsystem.py
@@ -1,17 +1,29 @@
 username = "immsteve"
 password = "123456"
 
+attempts = 0
+
 while True:
-    print("Welcome to THE USUAL LOGIN SYSTEM :)")
+        attempts += 1
+        print("Welcome to THE USUAL LOGIN SYSTEM :)")
 
-    actionuser = input("Enter your username: ")
-    actionpass = input("Enter your password: ")
+        actionuser = input("Enter your username: ")
+        actionpass = input("Enter your password: ")
 
-    if actionuser == username:
-        if actionpass == password:
-            print("Logged In successfully!")
+        success1 = actionuser == username
+        success2 = actionpass == password
+
+        if success1:
+            if success2:
+                print("Logged In successfully!")
+                break
+            else:
+                print("Incorrect Password!")
         else:
-            print("Incorrect Password!")
-    else:
-        print("User not found!")
-    
+            print("User not found!")
+        
+        if attempts == 3:
+             print("3 Wrong Tries")
+             break
+        else:
+             continue
